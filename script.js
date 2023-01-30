6f9e72d8907db9728a2676f08b4a0f2f8ec1d077
Amit Kumar <106420527+amitjait@users.noreply.github.com>
2023-01-30 22:06:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function convertToRoman(num) {
   //your code here
-	let num = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
+	let n = [1,4,5,9,10,40,50,90,100,400,500,900,1000];
     let sym = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"];
     let i=12;
     let ans = "";
     while(number>0)
     {
-    let div = Math.floor(number/num[i]);
-    number = number%num[i];
+    let div = Math.floor(number/n[i]);
+    number = number%n[i];
     while(div--)
     {
         // document.write(sym[i]);
